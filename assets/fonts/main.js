77cc3c7db9f24aa301ae07f44fba541963aee2f5
Tan Vo <tan.vo@apgandco.com>
2016-06-17 16:34:11 +1000

    Rejigged the navigation

---
+++ assets/fonts/main.js
@@ -0,0 +1,13 @@
+$(function() {
+    $('.toggle-nav').click(function() {
+        toggleNav();
+    });
+});
+
+function toggleNav() {
+    if ($('#site-wrapper').hasClass('show-nav')) {
+        $('#site-wrapper').removeClass('show-nav');
+    } else {
+        $('#site-wrapper').addClass('show-nav');
+    }
+}
